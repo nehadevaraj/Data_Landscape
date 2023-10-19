e3bcd72fc97a632ea9810170b68054ee80a5ebe1
nehadevaraj <ec22665@qmul.ac.uk>
2023-10-19 17:29:42 +0100

    importing

---
+++ main.py
@@ -0,0 +1,11 @@
+
+import requests
+import pandas as pd
+import numpy as np
+import os
+from dotenv import load_dotenv, find_dotenv #reading key values
+
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
+
+pd.set_option('display.max_columns', None)
