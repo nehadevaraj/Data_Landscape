a55ee759acd9c78ac551d0415b5f4d34e9247b97
Dmitrii Ponomarev <dima11102003@gmail.com>
2023-10-24 16:32:32 +0100

    DIMI made it work

--- test.py
+++ test.py
@@ -1,25 +1,23 @@
-
 import base64
 import webbrowser
 from urllib.parse import urlencode
-
+import random
 client_id = "bf9ae963bd6e4ba399cb411df7c2ed27"
 client_secret = "ef91f6595ba14e36b834f348d3acf68d"
 
 SCOPES = [
-        'ugc-image-upload',
-        'user-read-playback-state',
-        'user-top-read',
-        'user-read-recently-played',
-        'playlist-read-private',
-        'user-read-currently-playing'
+        "user-read-private",
+        "user-read-email",
+        "user-top-read",
+        "user-follow-read"
     ]
 
 auth_headers = {
     "client_id": client_id,
     "response_type": "code",
     "redirect_uri": "http://localhost:3000",
-    "scope": " ".join(SCOPES)
+    "scope": " ".join(SCOPES),
+    "state": random.randint(0,16)
 }
 
 webbrowser.open("https://accounts.spotify.com/authorize?" + urlencode(auth_headers))
